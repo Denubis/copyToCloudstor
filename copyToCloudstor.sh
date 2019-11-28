d042f7680ce48d78267ce81ca01609f8ae60270f
Brian Ballsun-Stanton <brian@fedarch.org>
2019-11-28 17:49:00 +1100

    shellcheck run

--- copyToCloudstor.sh
+++ copyToCloudstor.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+#Don't forget to run shellcheck (https://github.com/koalaman/shellcheck) after making edits.
 counter=1
 set -euo pipefail
+
+yours="-1"
+latest="-2"
 
 for line in $(rclone version --check | sed -E 's/:\s*/=/g' | sed -E 's/\s{2,}.*//g');
 do
 	if [[ $line == *"="* ]]; then
-		eval $line
+		eval "$line"
 	fi
 
 done
@@ -24,18 +29,18 @@ if [ "$#" -ne 2 ]; then
 	echo "./copyToCloudstor <src> <rcloneEndpoint:dest>"
 	exit 1
 fi
-source_absolute_path=$(readlink -m $1)
+source_absolute_path=$(readlink -m "$1")
 
 echo "Copying ${source_absolute_path} to ${2}. Starting at $(date)"
 
 
-while ! rclone check --one-way $source_absolute_path $2 2>&1 | grep ': 0 differences found'; 
+while ! rclone check --one-way "$source_absolute_path" "$2" 2>&1 | grep ': 0 differences found'; 
 do
 	counter=$((counter+1))
 	echo "Starting run ${counter} at $(date)"
-	rclone copy --progress --transfers 6 $source_absolute_path $2
+	rclone copy --progress --transfers 6 "$source_absolute_path" "$2"
 	echo "Done with run ${counter} at $(date)"
 done
 
 duration=$SECONDS
-echo "Copied ${source_absolute_path} to ${2}. Finished at $(date), in $(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+echo "Copied ${source_absolute_path} to ${2}. Finished at $(date), in ($duration / 60) minutes and ($duration % 60) seconds elapsed."
