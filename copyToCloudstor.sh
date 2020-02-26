f86a3f857922062db992cf7d3b4836efb6712b72
Michael D'Silva <michael.dsilva@aarnet.edu.au>
2020-02-26 13:59:38 +1100

    clean up, added showdiff option, grouped rclone options in var

--- copyToCloudstor.sh
+++ copyToCloudstor.sh
@@ -3,12 +3,16 @@
 set -euo pipefail
 
 #default values
-HELP=0
-VERSIONCHECK=1
-PUSHFIRST=0
+
 CHECK=1
+CHECKERS=36
 EXTRAVARS=0
-TRANSFERS=6
+HELP=0
+PUSHFIRST=0
+VERSIONCHECK=1
+SHOWDIFF=""
+TIMEOUT=0
+TRANSFERS=12
 
 #cli options
 POSITIONAL=()
@@ -19,14 +23,6 @@ while [[ $# -gt 0 ]]; do
 			HELP=1
 		    	shift # past argument
 	    	;;
-		--skipversioncheck)
-			VERSIONCHECK=0
-		    	shift # past argument
-	    	;;
-		--pushfirst)
-			PUSHFIRST=1
-		    	shift # past argument
-	    	;;
 		--nocheck)
 			CHECK=0
 		    	shift # past argument
@@ -35,6 +31,18 @@ while [[ $# -gt 0 ]]; do
 			TRANSFERS="${2}"
 		    	shift # past argument
 			shift # past value
+	    	;;
+		--pushfirst)
+			PUSHFIRST=1
+		    	shift # past argument
+	    	;;
+		--showdiff)
+			SHOWDIFF="-vv"
+		    	shift # past argument
+	    	;;
+		--skipversioncheck)
+			VERSIONCHECK=0
+		    	shift # past argument
 	    	;;
     		*)    # unknown option
 			EXTRAVARS=1
@@ -55,6 +63,7 @@ if [ "$#" -ne 2 ] || [ ${HELP} -eq 1 ]; then
 	echo "  --pushfirst         : Skip first oneway check (one less propfind)"
 	echo "  --nocheck           : Just pushes once without retrying"
 	echo "  -p|--parallel       : Number of file transfers to run in parallel. (default 6)"
+	echo "  --showdiff          : Show diff when checking for differences"
 
 	exit 1
 fi
@@ -76,16 +85,21 @@ source_absolute_path=$(readlink -m ${1})
 
 echo "Copying ${source_absolute_path} to ${2}. Starting at $(date)"
 
+rcloneoptions="--transfers ${TRANSFERS} --checkers ${CHECKERS} --timeout ${TIMEOUT}"
+
+counter=1
 if [ ${PUSHFIRST} -eq 1 ] || [ ${CHECK} -eq 0 ]; then
-	rclone copy --progress --no-traverse --transfers ${TRANSFERS} ${source_absolute_path} ${2}
+	echo "Starting run ${counter} at $(date) without checks"
+	rclone copy --progress --no-check-dest --no-traverse ${rcloneoptions} ${source_absolute_path} ${2}
+	echo "Done with run ${counter} at $(date)"
+	counter=$((counter+1))
 fi
 if [ ${CHECK} -eq 1 ]; then
-	counter=1
-	while ! rclone check --one-way ${source_absolute_path} ${2} 2>&1 | grep ': 0 differences found'; do
-		counter=$((counter+1))
+	while ! rclone check --one-way ${SHOWDIFF} ${rcloneoptions} ${source_absolute_path} ${2} 2>&1 | tee /dev/stderr | grep ': 0 differences found'; do
 		echo "Starting run ${counter} at $(date)"
-		rclone copy --progress --transfers ${TRANSFERS} ${source_absolute_path} ${2}
+		rclone copy --progress ${rcloneoptions} ${source_absolute_path} ${2}
 		echo "Done with run ${counter} at $(date)"
+		counter=$((counter+1))
 	done
 fi
 
